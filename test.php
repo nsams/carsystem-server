ed5710d3f9bca928fd920c3da8a77a213419bda1
Niko Sams <niko.sams@gmail.com>
2016-11-01 20:46:03 +0100

    test for servo commands

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+echo file_get_contents('http://10.0.0.101/servo.cgi?Nr=0&Pos=10000');
+//Nr ... Servo Nummer (0-23)
+//Pos ... Position (0-124)
+
+/*
+ Befehle:
+ * - position setzen
+ *   /servo.cgi?action=posset&Nr=0&Pos=1
+ * - initialposition setzen (TODO)
+ *   /servo.cgi?action=possetdefault&Nr=0&Pos=1
+ * - position abfragen (TODO)
+ *   /servo.cgi?action=posget&Nr=0
+ * 
+ * Antwort:
+ * 200: OK
+ * 500: Fehler (TODO)
+ */
